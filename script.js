ff0fc463fff806bc5969b828da0459c2c435f05f
Vadimgrig2012 <Vadim.grig2012@gmail.com>
2024-05-20 19:01:59 +0400

    add js code

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     // Select the button element from the document
     const button = document.querySelector('.btn');
+    const txtBtn = document.querySelector('.btn-text')
   
     // Get the button's initial bounding rectangle dimensions and position
     let boundingRect = button.getBoundingClientRect();
+    let boundingRectTxt = txtBtn.getBoundingClientRect();
+
   
     // Add an event listener for mouse movement over the button
     button.addEventListener('mousemove', (e) => {
       // Get the current bounding rectangle dimensions and position
       boundingRect = button.getBoundingClientRect();
+      boundingRectTxt = txtBtn.getBoundingClientRect();
   
       // Calculate mouse position relative to the button
       const mousePosX = e.clientX - boundingRect.left - boundingRect.width / 2; // X position relative to the center of the button
       const mousePosY = e.clientY - boundingRect.top - boundingRect.height / 2; // Y position relative to the center of the button
   
+      const mousePosTxtX = e.clientX - boundingRectTxt.left - boundingRectTxt.width / 2;
+      const mousePosTxtY = e.clientY - boundingRectTxt.top - boundingRectTxt.height / 2;
+
+      const zDisplacement = Math.max(-20, Math.min(20, (mousePosX / boundingRect.width) * 20));
+
       // Use GSAP (GreenSock Animation Platform) to animate the button
       gsap.to(button, {
         scale: 1.5,
         x: mousePosX * 2, // Move horizontally towards mouse
         y: mousePosY * 2, // Move vertically towards mouse
+        duration: 0.5, // Duration of the animation
+        ease: 'power3.out', // Easing function for smooth animation
+      });
+      gsap.to(txtBtn,{
+        x: mousePosX * 0.2, // Move horizontally towards mouse
+        y: mousePosY * 0.2, // Move vertically towards mouse
+        rotationZ: zDisplacement,
         duration: 0.5, // Duration of the animation
         ease: 'power3.out', // Easing function for smooth animation
       });
@@ -33,6 +49,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
         scale: 1, // Reset scale
         duration: 1, // Duration of the animation
         ease: 'elastic.out(1,0.3)' // Easing function for a 'springy' return
+      });
+      gsap.to(txtBtn, {
+        x: 0, // Reset horizontal position
+        y: 0, // Reset vertical position
+        scale: 1, // Reset scale
+        rotationZ: 0,
+        duration: 1, // Duration of the animation
+        ease: 'elastic.out(1,0.3)' 
       });
     });
   });
